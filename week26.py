a3b98483b4210e50b13191d0afc2c5ee60122347
Poompipat-Poom <111419818+Poompipatpoomnewton@users.noreply.github.com>
2023-02-08 11:46:47 +0700

    Create week26.py

---
+++ week26.py
@@ -0,0 +1,12 @@
+expression = input("please enter your number"
+x,y,z = expression.split ("")
+x = int(x)
+z = int(z)
+if y == "+":
+   print(x+z)
+elif y == "-":
+   print(x-z)
+elif y == "*"
+   print(x*z)
+else:
+   print(x/z)
